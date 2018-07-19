8e542a0a8c1d2479cbf0bf732ac573c7f9470397
Surpris <take90-it09-easy27@outlook.jp>
2018-07-20 03:17:16 +0900

    modify calc_EMA so that it takes time series and # of points to apply EMA with.

--- pybitcoin/gui/utils/mathfunctions.py
+++ pybitcoin/gui/utils/mathfunctions.py
@@ -2,8 +2,14 @@
 # -*- coding: utf-8 -*-
 from scipy.signal import lfilter
 
-def calc_EMA(x, alpha):
-    """ calc_EMA(x, alpha)
+def calc_EMA(x, N):
+    """calc_EMA(x, N) -> array-like
+    calculate exponential moving average (EMA)
+    """
+    return _calc_EMA(x, 2./(N + 1.))
+
+def _calc_EMA(x, alpha):
+    """ _calc_EMA(x, alpha) -> array-like
     Adopted from https://qiita.com/toyolab/items/6872b32d9fa1763345d8
     """
     y,_ = lfilter([alpha], [1,alpha-1], x, zi=[x[0]*(1-alpha)])
