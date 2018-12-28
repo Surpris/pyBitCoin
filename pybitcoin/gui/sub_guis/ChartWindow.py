cb2a8ebe899b081622ffc445f479baab2d9bfee8
Surpris <take90-it09-easy27@outlook.jp>
2018-12-29 00:47:33 +0900

    separate a plot area into two areas: for OHLC and for cross point
    add find_cross_points to mathfunctions.py

--- pybitcoin/gui/sub_guis/ChartWindow.py
+++ pybitcoin/gui/sub_guis/ChartWindow.py
@@ -8,9 +8,11 @@ This file offers the following items:
 * ChartWindow
 """
 import numpy as np
+import pandas as pd
 from PyQt5.QtWidgets import QMainWindow, QDialog, QGridLayout, QMenu, QWidget, QLabel, QLineEdit, QTextEdit
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtGui import QPainter
+from PyQt5.QtGui import QIntValidator, QDoubleValidator
 from PyQt5.QtCore import Qt
 # from PyQt5.QtChart import QChartView, QChart
 import pyqtgraph as pg
@@ -40,20 +42,38 @@ class ChartWindow(QDialog):
         """
         self._window_width = 600 # [pixel]
         self._window_height = 450 # [pixel]
+        self._spacing = 5 # [pixel]
         self._groupbox_title_font_size = 14
-        self._spacing = 5
         self._window_color = "gray"
+        self._txt_bg_color = "#D0D3D4"
 
         self._count = 0
         self._N_ema1 = 5
-        self._color_ema1 = "#3C8CE7"
         self._N_ema2 = 20
+        self._color_ema1 = "#3C8CE7"
         self._color_ema2 = "#EE8F1D"
+        self._color_cross_signal = "#FFFFFF"
         # self._plot_width = 30
         self.data = None
+        self.initInnerData()
+        self.updateAlpha()
+        self.DEBUG = True
+    
+    def initInnerData(self):
+        """initInnerData(self) -> None
+        """
         self._ohlc_list = []
         self._close = []
-        self.DEBUG = True
+        self._ema1 = []
+        self._ema2 = []
+        self._cross_signal = []
+
+    def updateAlpha(self):
+        """updateAlpha(self) -> None
+        update the alpha parameters for calulation of EMA
+        """
+        self._alpha1 = 2./(self._N_ema1 + 1.)
+        self._alpha2 = 2./(self._N_ema2 + 1.)
     
     def initGui(self):
         # self.setAttribute(Qt.WA_DeleteOnClose)
@@ -67,12 +87,15 @@ class ChartWindow(QDialog):
         
         # Plot area
         self.glw = pg.GraphicsLayoutWidget()
-        self.glw.resize(self._window_width - 20, self._window_width - 20)
+        self.glw.resize(self._window_width - 20, self._window_height - 20)
 
-        self.plot = self.glw.addPlot()
-        self.plot.plot()
-        
-        self.grid.addWidget(self.glw, 0, 0)
+        self.chart = self.glw.addPlot()
+        # self.chart.plot()
+
+        self.glw.nextRow()
+        self.chart2 = self.glw.addPlot()
+        self.chart2.setMaximumHeight(self._window_height // 4)
+        self.grid.addWidget(self.glw, 0, 0, 2, 1)
 
         # Buttons in debug mode
         if self.DEBUG:
@@ -80,11 +103,32 @@ class ChartWindow(QDialog):
                 self, self._window_width - 20, self._window_width - 20,
                 "DEBUG", self._groupbox_title_font_size, self._spacing
             )
+            self.le_start = QLineEdit(group_debug)
+            self.le_start.setText("0")
+            # font = self.le_start.font()
+            # font.setPointSize(self._button_font_size)
+            # self.le_start.setFont(font)
+            self.le_start.resize((self._window_width - 50)//3, 16)
+            self.le_start.setStyleSheet("background-color:{};".format(self._txt_bg_color))
+            self.le_start.setValidator(QIntValidator())
+
+            self.le_end = QLineEdit(group_debug)
+            self.le_end.setText("100")
+            # font = self.le_end.font()
+            # font.setPointSize(self._button_font_size)
+            # self.le_end.setFont(font)
+            self.le_end.resize((self._window_width - 50)//3, 16)
+            self.le_end.setStyleSheet("background-color:{};".format(self._txt_bg_color))
+            self.le_end.setValidator(QIntValidator())
+
             self.button1 = make_pushbutton(
                 self, 40, 16, "Update", 14, method=self.update, color=None, isBold=False
             )
-            grid_debug.addWidget(self.button1, 0, 0)
-            self.grid.addWidget(group_debug, 1, 0)
+            grid_debug.addWidget(self.le_start, 0, 0)
+            grid_debug.addWidget(self.le_end, 0, 1)
+            grid_debug.addWidget(self.button1, 0, 2)
+
+            self.grid.addWidget(group_debug, 2, 0, 1, 1)
     
     def initMainGrid(self):
         """ initMainWidget(self) -> None
@@ -100,48 +144,102 @@ class ChartWindow(QDialog):
         """update(self, data) -> None
         """
         try:
-            data_ = np.array(self.data[self._count % len(self.data)])
-            self._count += 1
-            if self._count > len(self.data):
-                data_[0] = self._count
-            self._ohlc_list.append(data_)
-            self._close.append(data_[-1])
-            self.plot.clear()
-            self.plot.addItem(CandlestickItem(self._ohlc_list))
-            self.plot.plot(
-                np.arange(self._ohlc_list[0][0], len(self._close)+self._ohlc_list[0][0]), 
-                calc_EMA(self._close, self._N_ema1), 
-                clear=False, pen=pg.mkPen(self._color_ema1, width=2)
-            )
-            self.plot.plot(
-                np.arange(self._ohlc_list[0][0], len(self._close)+self._ohlc_list[0][0]), 
-                calc_EMA(self._close, self._N_ema2), 
-                clear=False, pen=pg.mkPen(self._color_ema2, width=2)
-            )
+            start_ = int(self.le_start.text())
+            end_ = int(self.le_end.text())
+            data_ = self.data[["Open", "High", "Low", "Close"]].values[start_:end_].copy()
+            self.initInnerData()
+            buff = np.zeros(5)
+            for ii in range(len(data_)):
+                buff[0] = start_ + ii
+                buff[1:] = data_[ii].copy()
+                self.updateInnerData(buff.copy())
+            self.updatePlots()
         except Exception as ex:
             print(ex)
+    
+    def updateInnerData(self, data):
+        """updateInnerData(self, data) -> None
+        """
+        # self._count += 1
+        # if self._count > len(self.data):
+        #     data[0] = self._count
+        self._ohlc_list.append(data)
+        self._close.append(data[-1])
+
+        if len(self._ema1) == 0:
+            self._ema1.append(data[-1])
+        else:
+            self._ema1.append((1. - self._alpha1) * self._ema1[-1] + self._alpha1 * data[-1])
+
+        if len(self._ema2) == 0:
+            self._ema2.append(data[-1])
+        else:
+            self._ema2.append((1. - self._alpha2) * self._ema2[-1] + self._alpha2 * data[-1])
+
+        self._cross_signal.append(self.judgeCrossPoint())
+    
+    def judgeCrossPoint(self):
+        """judgeCrossPoint(self) -> float
+        """
+        if len(self._ema1) == 0 or len(self._ema2) == 0:
+            raise ValueError("Some error occurs on the inner data.")
+        if len(self._ema1) == 1 or len(self._ema2) == 1:
+            return 0.
+        
+        ema1_before, ema1_current = self._ema1[-2], self._ema1[-1]
+        ema2_before, ema2_current = self._ema2[-2], self._ema2[-1]
+        if ema1_before >= ema2_before and ema1_current < ema2_current:
+            return -1.
+        elif ema1_before < ema2_before and ema1_current >= ema2_current:
+            return 1.
+        else:
+            return 0.
+    
+    def updatePlots(self):
+        """updatePlots(self) -> None
+        """
+        self.chart.clear()
+        self.chart.addItem(CandlestickItem(self._ohlc_list))
+        self.chart.plot(
+            np.arange(self._ohlc_list[0][0], len(self._close)+self._ohlc_list[0][0]), self._ema1, 
+            clear=False, pen=pg.mkPen(self._color_ema1, width=2)
+        )
+        self.chart.plot(
+            np.arange(self._ohlc_list[0][0], len(self._close)+self._ohlc_list[0][0]), 
+            calc_EMA(self._close, self._N_ema2), 
+            clear=False, pen=pg.mkPen(self._color_ema2, width=2)
+        )
+        self.chart2.plot(
+            np.arange(self._ohlc_list[0][0], len(self._close)+self._ohlc_list[0][0]), 
+            self._cross_signal,
+            clear=False, pen=pg.mkPen(self._color_cross_signal, width=2)
+        )
 
     def setData(self, data):
         """setData(self, data) -> None
         """
-        if isinstance(data, np.ndarray):
-            data = np.array(data)
-        self.data = data
+        if isinstance(data, pd.DataFrame):
+            self.data = data
+        elif isinstance(data, str):
+            self.data = pd.read_csv(data, index_col=0)
+        else:
+            raise TypeError("The parameter 'data' must have either DataFrame or str.")
         self._count = 0
 
 def main():
     app = QApplication([])
     # app.setWindowIcon(QIcon(os.path.join(os.path.dirname(__file__), "python.png")))
     mw = ChartWindow()
-    data = [  ## fields are (time, open, close, min, max).
-        (1., 10, 15, 5, 13),
-        (2., 13, 20, 9, 17),
-        (3., 17, 23, 11, 14),
-        (4., 14, 19, 5, 15),
-        (5., 15, 22, 8, 9),
-        (6., 9, 16, 8, 15),
-    ]
-    mw.setData(data)
+    # data = [  ## fields are (time, open, close, min, max).
+    #     (1., 10, 15, 5, 13),
+    #     (2., 13, 20, 9, 17),
+    #     (3., 17, 23, 11, 14),
+    #     (4., 14, 19, 5, 15),
+    #     (5., 15, 22, 8, 9),
+    #     (6., 9, 16, 8, 15),
+    # ]
+    fpath = r'..\data\OHLC_20181211.csv'
+    mw.setData(fpath)
     mw.show()
     app.exec_()
 
--- pybitcoin/gui/utils/mathfunctions.py
+++ pybitcoin/gui/utils/mathfunctions.py
@@ -1,5 +1,7 @@
 #! /usr/bin/python3
 # -*- coding: utf-8 -*-
+
+import numpy as np
 from scipy.signal import lfilter
 
 def calc_EMA(x, N):
@@ -14,3 +16,54 @@ def _calc_EMA(x, alpha):
     """
     y,_ = lfilter([alpha], [1,alpha-1], x, zi=[x[0]*(1-alpha)])
     return y
+
+def find_cross_points(y1, y2=None):
+    """find_cross_points(y1, y2=None) -> numpy.1darray
+    find cross points between two data sequences
+    If y2 == None, then find the root of y1.
+    
+    Parameters
+    ----------
+    y1 : list or numpy.1darray
+    y2 : list or numpy.1darray
+    
+    Returns
+    -------
+    cross_points : numpy.1darray
+    """
+    if y2 is None:
+        y2 = np.zeros_like(y1)
+    if isinstance(y1, list):
+        y1 = np.array(y1)
+    if isinstance(y2, list):
+        y2 = np.array(y2)
+    cross_points = np.zeros(len(y1))
+    for ii in range(1, len(y1)):
+        if y1[ii - 1] >= y2[ii - 1] and y1[ii] < y2[ii]:
+            cross_points[ii] = -1
+        elif y1[ii - 1] < y2[ii - 1] and y1[ii] >= y2[ii]:
+            cross_points[ii] = 1
+    return cross_points
+
+def symbolize(dataFrame, k):
+    """symbolize(dataFrame, k) -> numpy.1darray
+    binalize a k-length OHLC dataset and then convert the binary to decimal number.  
+    
+    Parameters
+    ----------
+    dataFrame : pandas.DataFrame
+    k : int
+    
+    Returns
+    -------
+    dec : numpy.1darray
+    """
+    if k <= 0:
+        raise ValueError("k must be >=1.")
+    var_ = (dataFrame["Close"] - dataFrame["Open"]).values
+    dec = np.zeros(len(var_), int)
+    
+    for ii in range(k-1, len(var_)):
+        ind_ = np.array(var_[ii-k+1:ii+1] >= 0, int)
+        dec[ii] = int("".join([str(i_) for i_ in ind_]), 2)
+    return dec
