b343efa7fbc158d58f1bda583be2414de6d6f07d
Surpris <take90-it09-easy27@outlook.jp>
2019-01-13 20:50:42 +0900

    add peakdet function to mathfunctions.py, add analysis_functions.py to the submodule "utils"

--- pybitcoin/gui/utils/__init__.py
+++ pybitcoin/gui/utils/__init__.py
@@ -1,9 +1,10 @@
 #! /usr/bin/python3
 # -*- coding: utf-8 -*
 
+from .analysis_functions import analyze
 from .decorator import dynamic_decorator
 from .footprint import footprint
 from .get_logger import get_logger
-from .mathfunctions import calc_EMA
+from .mathfunctions import calc_EMA, find_cross_points, symbolize, peakdet
 from .rategetter import get_rate_via_crypto, to_dataFrame
 from .widget_wrapper import make_groupbox_and_grid, make_label, make_pushbutton
