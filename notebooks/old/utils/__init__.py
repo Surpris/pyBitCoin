2e203e58e73b54402c3da86fd50eb5b287c6775b
Surpris <take90-it09-easy27@outlook.jp>
2019-01-27 20:05:27 +0900

    move old notesbooks to "old/"

---
+++ notebooks/old/utils/__init__.py
@@ -0,0 +1,3 @@
+#-*- coding: utf-8 -*-
+
+from .utils import find_cross_points, calc_EMA, calc_SMA, symbolize, peakdet
---
+++ notebooks/old/utils/utils.py
@@ -0,0 +1,184 @@
+#-*- coding: utf-8 -*-
+
+from datetime import datetime
+# import itertools
+import numpy as np
+import pandas as pd
+from scipy.signal import lfilter
+import sys
+
+def calc_EMA(x, N=5):
+    """calc_EMA(x, N) -> array-like
+    calculate exponential moving average (EMA)
+    
+    Parameters
+    ----------
+    x : array-like
+        target data series
+    N : int (default : 5)
+        step for EMA
+    
+    Returns
+    -------
+    EMA : array-like
+    """
+    return _calc_EMA(x, 2./(N + 1.))
+
+def _calc_EMA(x, alpha):
+    """ _calc_EMA(x, alpha) -> array-like
+    calculate exponential moving average (EMA)
+    Adopted from https://qiita.com/toyolab/items/6872b32d9fa1763345d8
+    
+    Parameters
+    ----------
+    x : array-like
+        target data series
+    alpha : float
+        characteristic parameter
+    
+    Returns
+    -------
+    EMA : array-like
+    """
+    y,_ = lfilter([alpha], [1,alpha-1], x, zi=[x[0]*(1-alpha)])
+    return y
+
+def calc_SMA(df, key="Close", N=5):
+    """calc_SMA(df, key="Close", N=5) -> numpy.1darray
+    calculate simple moving average (SMA)
+    
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        dataframe including target data
+    key : str (default : 'Close')
+        key to the target data
+    N : int (default : 5)
+        step for SMA
+    
+    Returns
+    -------
+    SMA : numpy.1darray
+    """
+    return df[key].rolling(5).mean().values
+
+def find_cross_points(y1, y2=None):
+    """find_cross_points(y1, y2=None) -> numpy.1darray
+    find cross points between two data sequences
+    If y2 == None, then find the root of y1.
+    
+    Parameters
+    ----------
+    y1 : list or numpy.1darray
+    y2 : list or numpy.1darray
+    
+    Returns
+    -------
+    cross_points : numpy.1darray
+    """
+    if y2 is None:
+        y2 = np.zeros_like(y1)
+    if isinstance(y1, list):
+        y1 = np.array(y1)
+    if isinstance(y2, list):
+        y2 = np.array(y2)
+    cross_points = np.zeros(len(y1))
+    for ii in range(1, len(y1)):
+        if y1[ii - 1] >= y2[ii - 1] and y1[ii] < y2[ii]:
+            cross_points[ii] = -1
+        elif y1[ii - 1] < y2[ii - 1] and y1[ii] >= y2[ii]:
+            cross_points[ii] = 1
+    return cross_points
+
+def symbolize(dataFrame, k):
+    """symbolize(dataFrame, k) -> numpy.1darray
+    binalize a k-length OHLC dataset and then convert the binary to decimal number.  
+    
+    Parameters
+    ----------
+    dataFrame : pandas.DataFrame
+    k : int
+    
+    Returns
+    -------
+    dec : numpy.1darray
+    """
+    if k <= 0:
+        raise ValueError("k must be >=1.")
+    var_ = (dataFrame["close"] - dataFrame["open"]).values
+    dec = np.zeros(len(var_), int)
+    
+    for ii in range(k-1, len(var_)):
+        ind_ = np.array(var_[ii-k+1:ii+1] >= 0, int)
+        dec[ii] = int("".join([str(i_) for i_ in ind_]), 2)
+    return dec
+
+def peakdet(v, delta, x=None):
+    """peakdet(v, delta, x=None) -> numpy.2darray, numpy.2darray
+    Converted from MATLAB script at http://billauer.co.il/peakdet.html
+    returns two arrays which include the local maxima and the local minima.
+    function [maxtab, mintab]=peakdet(v, delta, x)
+    % PEAKDET Detect peaks in a vector
+    % [MAXTAB, MINTAB] = PEAKDET(V, DELTA) finds the local
+    % maxima and minima ("peaks") in the vector V.
+    % MAXTAB and MINTAB consists of two columns. Column 1
+    % contains indices in V, and column 2 the found values.
+    %
+    % With [MAXTAB, MINTAB] = PEAKDET(V, DELTA, X) the indices
+    % in MAXTAB and MINTAB are replaced with the corresponding
+    % X-values.
+    %
+    % A point is considered a maximum peak if it has the maximal
+    % value, and was preceded (to the left) by a value lower by
+    % DELTA.
+    % Eli Billauer, 3.4.05 (Explicitly not copyrighted).
+    % This function is released to the public domain; Any use is allowed.
+    
+    Parameters
+    ----------
+    v     : array-like
+    delta : numeric
+    x     : array-like (default : None)
+    
+    Returns
+    -------
+    maxtab : numpy.2darray
+    mintab : numpy.2darray
+    """
+    maxtab = []
+    mintab = []
+    if x is None:
+        x = np.arange(len(v))
+        v = np.asarray(v)
+    if len(v) != len(x):
+        sys.exit('Input vectors v and x must have same length')
+    if not np.isscalar(delta):
+        sys.exit('Input argument delta must be a scalar')
+    if delta <= 0:
+        sys.exit('Input argument delta must be positive')
+    mn, mx = np.Inf, -np.Inf
+    mnpos, mxpos = np.NaN, np.NaN
+    lookformax = True
+
+    for i in np.arange(len(v)):
+        this = v[i]
+        if this > mx:
+            mx = this
+            mxpos = x[i]
+        if this < mn:
+            mn = this
+            mnpos = x[i]
+        if lookformax:
+            if this < mx - delta:
+                maxtab.append((mxpos, mx))
+                mn = this
+                mnpos = x[i]
+                lookformax = False
+        else:
+            if this > mn + delta:
+                mintab.append((mnpos, mn))
+                mx = this
+                mxpos = x[i]
+                lookformax = True
+
+    return np.array(maxtab), np.array(mintab)
